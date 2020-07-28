6d0ebb6bd46081e2a7f130f7a32ff66abfe7e2e2
matheus.lima <matheus.lima@avancoinfo.com.br>
2020-07-27 21:54:27 -0300

    commit do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../../assets/logo.png'
+import './Menu.css'
+import ButtonLink from './components/ButtonLink';
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Web Flix Logo"></img>
+            </a>
+            <ButtonLink className="ButtonLink" href="/">Novo vídeo</ButtonLink>
+        </nav>
+    )
+}
+
+export default Menu
